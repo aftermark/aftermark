ae2c0ab6365d9d1e46ac2713dd74ec9fd0a7d437
tinymachine <mihira@tinymachine.net>
2019-06-20 10:44:05 -0700

    fix(longlist-tagger): set defaults properly

--- packages/plugins/longlist-tagger/src/main.js
+++ packages/plugins/longlist-tagger/src/main.js
@@ -1,12 +1,15 @@
-export default function(dom, options) {
-  const minItemCount = options.minItemCount || 6;
-  const className = options.className || "long-list";
-
+export default function(
+  dom,
+  options = {
+    minItemCount: 6,
+    className: "long-list"
+  }
+) {
   const lists = dom.window.document.querySelectorAll("ul, ol");
 
   lists.forEach(list => {
-    list.querySelectorAll("li").length >= minItemCount &&
-      list.classList.add(className);
+    list.querySelectorAll("li").length >= options.minItemCount &&
+      list.classList.add(options.className);
   });
 
   return dom;
